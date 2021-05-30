48b668d321250073c6ca52c607d2dd42d313a2d9
michaeljwoloszyn <woloszym@sheridancollege.ca>
2021-05-29 23:01:51 -0400

    added java bean

---
+++ src/main/java/sheridan/woloszym/exercise1v1/domain/User.java
@@ -0,0 +1,33 @@
+package sheridan.woloszym.exercise1v1.domain;
+
+import java.io.Serializable;
+
+public class User implements Serializable {
+
+    private String name = "";
+    private String status = "";
+
+    public User() {
+    }
+
+    public User(String name, String status) {
+        this.name = name;
+        this.status = status;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
